f143ad661fdcf001882ee4379b7e166ca333ce46
ErickSwolkin <swolkinerick@gmail.com>
2023-08-16 13:55:26 +0200

    convercao de estilizacao CSS -> Styled Components

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import { EstiloCabecalho } from './style'
+
+const Cabecalho = () => (
+  <EstiloCabecalho>
+    <h1>EBAC Jobs</h1>
+  </EstiloCabecalho>
+)
+
+export default Cabecalho
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { FormularioHero, ContainerDiv, HeroTitle } from './style'
+
+const Hero = () => (
+  <FormularioHero>
+    <ContainerDiv>
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </ContainerDiv>
+  </FormularioHero>
+)
+
+export default Hero
